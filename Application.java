7337630f48fee6555dc79e2718cf324a6d4305c3
Ashutosh Ayush <jbnashu297@gmail.com>
2025-08-25 22:55:10 +0530

    Java Frame added

--- Application.java
+++ Application.java
@@ -2,6 +2,9 @@ package TODO_LIST;
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
-public class Application {
-  
+public class Application extends Jframe implements ActionListener {
+    @Override
+  public void actionPerformed(ActionEvent ae){
+
+  }
 }
