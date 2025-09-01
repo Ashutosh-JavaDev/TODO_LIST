9597827713216966d9cce625763a8f0bae80aa62
Ashutosh Ayush <jbnashu297@gmail.com>
2025-09-01 22:09:26 +0530

    Java  TODO Application

--- Application.java
+++ Application.java
@@ -8,16 +8,20 @@ import java.util.Date;
 import javax.swing.JFormattedTextField;
 
 public class Application extends JFrame implements ActionListener {
-
+  JLabel title;
+    public void addFunction(){
+      
+    }
   public Application() {
     setTitle("TODO_Application");
-    JLabel title=new JLabel("Hey,Welcome to the TODO Application");
-    title.setBounds(300, 20,300,20);
-    Font titleFont=new Font("Arial",Font.BOLD,20);
+    title=new JLabel("Hey,Welcome to the TODO Application");
+    title.setBounds(250,20,400,25);
+    Font titleFont=new Font("Arial",Font.BOLD,18);
     title.setFont(titleFont);
+    title.setForeground(Color.RED);
     add(title);
     // Fix Function
-    setLocation(400, 200);
+    setLocation(400,100);
     setLayout(null);
     setSize(800, 800);
     setVisible(true);
