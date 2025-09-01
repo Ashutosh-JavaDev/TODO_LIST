63074f68ccd678a7e485ee935b22d6c247b0db7d
Ashutosh Ayush <jbnashu297@gmail.com>
2025-09-01 15:26:55 +0530

    TODO List Application

--- Application.java
+++ Application.java
@@ -1,10 +1,30 @@
 package TODO_LIST;
+
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
-public class Application extends JFrame implements ActionListener {
-    @Override
-  public void actionPerformed(ActionEvent ae){
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.swing.JFormattedTextField;
 
+public class Application extends JFrame implements ActionListener {
+
+  public Application() {
+    setTitle("TODO_Application");
+    // Fix Function
+    setLocation(400, 200);
+    setLayout(null);
+    setSize(800, 800);
+    setVisible(true);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent ae) {
+
+  }
+
+  public static void main(String[] args) {
+    new Application();
   }
 }
